48d37c6ff84ae63c45056a66d84fc97abc5e93ed
Nejah Ben Nasrallah <nejahbn@gmail.com>
2025-03-26 22:17:29 +0100

    adjust LABREPO

--- build-lab.sh
+++ build-lab.sh
@@ -2,23 +2,33 @@
 
 export SEEDHOST="192.168.50.71"
 export SEEDUSER="admin"
+export LABREPO=/home/admin/lab
 
 sudo dnf install -y curl openssh-clients
 sudo dnf install -y ansible-core
 
 # create admin user locally
+echo "creating 'admin' user locally"
 sudo adduser admin
 sudo usermod -aG wheel admin
 
 # set admin password
+echo "setting password for the admin user"
 sudo passwd admin
 
-#download the imagemode VM template image to /tmp/lab/image-mode.qcow2
-scp -r $SEEDUSER@$SEEDHOST:lab/ /tmp/
-gunzip /tmp/lab/image-mode.qcow2.gz
+#download  lab artifacts to  LABREPO
 
+echo "Downloading lab artifacts from the seed host"
+echo "Please provide the passwprd of the SEEDUSER on SEEDHOST"
+scp -r $SEEDUSER@$SEEDHOST:lab/ $LABREPO
+sudo chown -R admin:admin $LABREPO
+gunzip $LABREPO/image-mode.qcow2.gz
+
+echo "Run ansible playbooks"
+echo "Please provide the credentials of the local 'admin' user"
 ansible-playbook -kK -i hosts configure-laptops.yml
 
+echo "Provision lab virtual machines"
 sudo qemu-img create -f qcow2 /imagemode/image-mode-test.qcow2 1G
 sudo qemu-img create -f qcow2 /imagemode/image-mode.qcow2 1G
 
@@ -26,10 +36,11 @@ sudo qemu-img create -f qcow2 /imagemode/image-mode.qcow2 1G
 #!!!!!!!!!!!!!! Overwrite default network !!!!!!!!!!!!!!!!
 #!!!!!!!!!!!!!! Current configuration will be LOST !!!!!!!
 sudo virsh net-undefine default
-sudo virsh create /tmp/lab/net-default.xml
+sudo virsh create $LABREPO/net-default.xml
 #----------------------------------------------------------
-sudo virsh create /tmp/lab/vm-imagemode.xml
-sudo virsh create /tmp/lab/vm-imagemodetest.xml
+sudo virsh create $LABREPO/vm-imagemode.xml
+sudo virsh create $LABREPO/vm-imagemodetest.xml
+
 
 
 
