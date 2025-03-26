5e2997e49fd53c0e4d4742c15affc324bdb52d62
Nejah Ben Nasrallah <nejahbn@gmail.com>
2025-03-26 23:36:10 +0100

    update virsh commands

--- build-lab.sh
+++ build-lab.sh
@@ -48,10 +48,15 @@ sudo qemu-img create -f qcow2 /imagemode/image-mode.qcow2 1G
 #---------------------------------------------------------
 #!!!!!!!!!!!!!! Overwrite default network !!!!!!!!!!!!!!!!
 #!!!!!!!!!!!!!! Current configuration will be LOST !!!!!!!
+sudo virsh net-destroy default
 sudo virsh net-undefine default
-sudo virsh create $LABREPO/net-default.xml
+sudo virsh net-create $LABREPO/net-default.xml
 #----------------------------------------------------------
+sudo virsh destroy imagemode
+sudo virsh undefine imagemode
 sudo virsh create $LABREPO/vm-imagemode.xml
+sudo virsh destroy image-mode-test
+sudo virsh undefine image-mode-test
 sudo virsh create $LABREPO/vm-imagemodetest.xml
 
 
